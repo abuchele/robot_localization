a343a0a727657a5b1643a83ad95d3fa38e129d2b
arianaolson419 <arianaolson419@gmail.com>
2018-10-02 14:02:34 -0400

    Update methods to match sensor api

--- robot_localizer/scripts/particle.py
+++ robot_localizer/scripts/particle.py
@@ -8,7 +8,11 @@ from helper_functions import TFHelper
 from occupancy_field import OccupancyField
 
 class Particle(object):
-    """ Represents a particle """
+    """ Represents a particle.
+    Atrributes
+    ----------
+    position: a Pose representing the position of the particle on the map frame.
+    """
     def __init__(self, position, weight, sensor_model):
         # TODO: add documentation to describe what these attributes are.
         self.position = position
@@ -16,15 +20,19 @@ class Particle(object):
         self.sensor_model = sensor_model
 
     def integrate_observation(self, observation):
-        """ integrate an observation """
-        # TODO: integrate more than just front/back observations.
-        self.weight *= self.sensor_model.get_likelihood(observation.north_laser, self.position, 1)
-        self.weight *= self.sensor_model.get_likelihood(observation.south_laser, self.position, -1)
+        """ integrate an observation and update the weight of the particle.
+        
+        Parameters
+        ----------
+        observation: a list of tuples of LIDAR readings and the angle at which they came from.
+        """
+        for distance, angle in observation:
+            self.weight *= self.sensor_model.get_likelihood(self.position, distance, angle)
 
     def predict(self, delta):
         """ predict the next position based on the delta measured using
             the odometry """
-        self.position  = self.sensor_model.sample_prediction(self.position+delta)
+        self.position  = self.sensor_model.sample_prediction(self.position, delta)
 
     def normalize_weight(self, Z):
         """ adjust the particle weight using the specified
